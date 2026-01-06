b7e3b356660b5e9982b6d3f11ea5dc68400fbde5
Edwin Ramos <edwin_ramos2@usmp.pe>
2026-01-05 22:27:05 -0500

    serializadores anidados

--- pacientes/serializers.py
+++ pacientes/serializers.py
@@ -1,10 +1,23 @@
-from    rest_framework import serializers
-from    .models import Paciente, SeguroMedico, ExpedienteMedico
+from rest_framework import serializers
+from .models import Paciente, SeguroMedico, ExpedienteMedico
+from reservas.serializers import CitaSerializer
 
 class PacienteSerializer(serializers.ModelSerializer):
+
+    citas = CitaSerializer(many=True, read_only=True) # many=True permite procesas varios elementos de ese mismo tipo, porque un paciente puede tener múltiples citas, read_only significa que este campo es solo de lectura y no se espera que se proporcione al crear o actualizar un paciente, solo se mostraran las citas asociadas al paciente.
     class Meta:
         model = Paciente
-        fields = '__all__'
+        fields = [
+        'id',    
+        'nombre',
+        'apellido',    
+        'fecha_nacimiento',
+        'telefono',
+        'correo',
+        'direccion', 
+        'historial_medico',
+        'citas'
+        ]
 
 class SeguroMedicoSerializer(serializers.ModelSerializer):
     class Meta:
