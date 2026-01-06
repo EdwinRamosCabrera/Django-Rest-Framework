94de76d735ceebc8d6127231a049d9e72585079c
Edwin Ramos <edwin_ramos2@usmp.pe>
2026-01-05 20:28:58 -0500

    validaciones

--- doctores/serializers.py
+++ doctores/serializers.py
@@ -5,6 +5,16 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = '__all__'
+    
+    def validate_correo(self, value):
+        if '@example.com' not in value:
+            raise serializers.ValidationError("El correo electrónico debe pertenecer al dominio '@example.com'.")
+        return value
+    
+    def validate(self, attrs):
+        if len(attrs['telefono']) > 10 and attrs['is_on_vacation']:
+            raise serializers.ValidationError("Por favor, ingresa un número de teléfono válido y antes de salir de vacaciones.")
+        return super().validate(attrs) # continua con la funcionalidad de la clase padre
 
 class DepartamentoSerializer(serializers.ModelSerializer):
     class Meta:
--- doctores/viewsets.py
+++ doctores/viewsets.py
@@ -14,7 +14,7 @@ class DoctorViewSet(viewsets.ModelViewSet):
     """
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsDoctor] # Si se autentica, solo puede leer. Si se autentica, debe ser doctor para modificar.
+    permission_classes = [IsAuthenticated, IsDoctor] # Solo usuarios autenticados y con rol de doctor pueden acceder.
 
     # Creacion de acciones personalizadas
     @action(detail=True, methods=['post'], url_path='set-on-vacation') # Al usar detail=True, esta acción se aplica a una instancia específica. Al usar viewsets unimos todas las vistas en una sola clase, por lo tanto, no se sabe si se está trabajando con una lista o con un detalle, por ello se especifica detail=True y este espera un id.
@@ -37,5 +37,3 @@ class DoctorViewSet(viewsets.ModelViewSet):
         doctor.save()
         return Response({"status": "El doctor NO esta en vacaciones"})
     
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
